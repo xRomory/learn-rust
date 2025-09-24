de022464392f8d2fa64b46eac676704356cce72c
jacob <romory@pop-os.CG-24D>
2025-09-24 18:17:11 +0800

    Guessing game done

--- beginner/day_one/guessing_game/src/main.rs
+++ beginner/day_one/guessing_game/src/main.rs
@@ -6,25 +6,32 @@ use rand::Rng;
 fn main() {
   println!("Guess the number!");
 
-  // Using `rand` library we can use functions such as random_range
   let secret_number = rand::rng().random_range(1..=100);
 
-  println!("The secret number is: {secret_number}");
+  // This causes an infinite loop (`while` loop on other programming languages)
+  loop {
+    println!("Please input your guess:");
 
-  println!("Please input your guess.");
+    let mut guess = String::new();
 
-  let mut guess = String::new();
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read line");
 
-  io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
 
-  println!("You guessed: {guess}");
+    println!("You guessed: {guess}");
 
-  // Will fix later. Not matching an integer
-  match guess(&secret_number) {
-    Ordering::Less => println!("Too small!"),
-    Ordering::Greater => println!("Too big!"),
-    Ordering::Equal => println!("You win!"),
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      },
+    }
   }
 }
