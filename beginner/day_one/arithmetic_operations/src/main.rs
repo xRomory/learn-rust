fb4951bae49521c461c53dc36c761f15394fb1f6
jacob <romory@pop-os.CG-24D>
2025-09-24 19:42:10 +0800

    Rust variables introduction

--- beginner/day_one/arithmetic_operations/src/main.rs
+++ beginner/day_one/arithmetic_operations/src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     let x = ...
     x = ... 
 
-    * This cause can error since when you use `let`, the variable becomes immutable
+    * This cause can error since when you use `let`, 
+    the variable becomes immutable
   */
 
   let x = 6;
@@ -26,9 +27,18 @@ fn main() {
   /*
     Shadowing:
 
-    This allows you to declare a new variable with the same name as a previous variable.
+    This allows you to declare a new variable 
+    with the same name as a previous variable.
+
+    Since it's basically a new variable,
+    we can assign a new data type (see ex. 2).
+
+    Things will be different if the variable is mutable.
+    It will caused a compile-error since you're basically
+    using the same variable (see ex. 3).
   */
 
+  // Ex. 1 
   let x = 5;
   println!("{x}");
 
@@ -41,4 +51,15 @@ fn main() {
 
   println!("Value of x: {x}");
 
+  // Ex. 2
+  let spaces = "   "; //string type
+  let spaces = spaces.len();  //number type
+
+  println!("{spaces}");
+
+  // Ex. 3
+  let mut numbers = "123";
+  println!("{numbers}");
+  
+  numbers = 123; // Error: expected &str
 }
