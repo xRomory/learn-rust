61d314148e0e65a63317053e66e93944a2cb7611
jacob <romory@pop-os.CG-24D>
2025-09-26 11:21:48 +0800

    Data types lesson

--- beginner/day_two/data_types/src/main.rs
+++ beginner/day_two/data_types/src/main.rs
@@ -45,8 +45,11 @@ fn main() {
   /*
     Numeric Operations:
 
-    Just like on other programming languages,
+    Just like in other programming languages,
     you can perform arithmetic operations on Rust.
+
+    Integer divisions truncates toward zero to the
+    nearest integer.
   */
 
   let sum = 5 + 10; // Addition
@@ -55,10 +58,75 @@ fn main() {
 
   // Division
   let quotient = 1 / 2;
-  let truncated = -6 / 2;
+  let truncated = -7 / 2; // Results in -3
 
   let remainder = 40 % 5;
 
-  println!()
+  println!("Sum: {sum}\nDifference: {difference}\nProduct: {product}\nQuotient: {quotient}");
+  println!("Truncated: {truncated}\nRemainder: {remainder}");
 
+  /* 
+    Boolean Type
+
+    Same syntax to Python, same fucntions sa other PL
+  */
+
+  let t = true;
+  let f: bool = false;
+
+  /* 
+    Character Type
+
+    * Same practice to Java, char literals nakalagay sa single quotes
+    * 4-bytes in size, and represents a Unicode scalar value.
+  */
+
+  let char_non_explicit = 'z';
+  let char_explicit: char = 'Z';
+  let heart_eyed_cat = '😻'; // We can do this since unicode
+
+  /* 
+    Compound Type
+
+    There are two primitive compound types: Tuples and Arrays.
+
+    With Tuples, it is considered as a single compound element.
+    * Same sa other languages, index counting rin siya
+    * If tuple don't have explicit values, it is called: `unit`
+
+    With array, every elements must have the same type.
+    Sa Rust, arrays have a fixed length.
+  */
+
+  // Tuples
+  let tuple: (i32, f32, u8) = (-500, 0.4, 1);
+
+  // We can pass `tuple` like this:
+  let (a, b, c) = tuple;
+
+  println!("The value of a: {a}\nThe value of b: {b}\nThe value of c: {c}");
+
+  let negative_five_hundred = tuple.0;
+  let point_four = tuple.1;
+  let one = tuple.2;
+
+  println!("First index: {negative_five_hundred}");
+  println!("Second index: {point_four}");
+  println!("Third index: {one}");
+
+  // Arrays
+  let arr = [1, 2, 3, 4, 5];
+  println!("Array: {:?}", arr); // When printing an array, use `:?` debug format specifier
+
+  // We can also do this if the same values yung elements
+  // value: 3, length: 5
+
+  let three_five = [3; 5];
+  println!("{:?}", three_five);
+
+  // Same with other PLs when accessing array elements
+  let first_value = arr[0];
+  let last_value = arr[4];
+
+  println!("First value: {first_value} \nLast value: {last_value}");
 }
