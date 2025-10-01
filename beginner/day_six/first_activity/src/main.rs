1b1ee7053a7b019ed3cd4370a58c86d1bd8ca9ca
jacob <romory@pop-os.CG-24D>
2025-10-01 19:19:37 +0800

    Initial Commit

---
+++ beginner/day_six/first_activity/src/main.rs
@@ -0,0 +1,32 @@
+/*
+    Personal Expense Tracker (Beginner)
+
+    * The purpose of this project is to apply what I've learned so far.
+        * Scope and Variables
+        * Data Types (Arithmetic Functions)
+        * Loopings
+        * Ownership
+        * Borrowing and References
+    
+    * Project Specification:
+        * Basic lang talaga, add expenses
+        * Display yung expenses niya
+        * Transaction history
+            * Sorted by date, or category
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn add_expense() {
+    // Implement
+}
+
+fn display_expenses() {
+    // Logic for displaying
+}
+
+fn transaction_history() {
+    // Logic here
+}
