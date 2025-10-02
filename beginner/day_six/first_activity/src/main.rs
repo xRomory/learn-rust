b6a4814e3a23600f0c04ca0e8f45c54d6076b5ee
jacob <romory@pop-os.CG-24D>
2025-10-02 14:48:52 +0800

    Main Program Flow done.

--- beginner/day_six/first_activity/src/main.rs
+++ beginner/day_six/first_activity/src/main.rs
@@ -13,10 +13,56 @@
         * Display yung expenses niya
         * Transaction history
             * Sorted by date, or category
+            * For Category, I should limit it to prevent user input error
 */
+use std::io;
+
+#[derive(Debug)]
+struct Expenses {
+    item_name: String,
+    price: f64,
+    category: String,
+    date: String,
+}
 
 fn main() {
-    println!("Hello, world!");
+    // let mut expense: Vec<Expenses> = Vec::new();
+
+    println!("=== Personal Expense Tracker ===");
+
+    loop {
+        println!("\nMain Menu:");
+        println!("1. Calculate Expenses");
+        println!("2. View Total Expenses (This Month)");
+        println!("3. Transaction History");
+        println!("4. Exit");
+        println!("Choose between (1-4): ");
+
+        let mut user_input = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to Read Line");
+
+        let choice = match user_input.trim().parse::<i8>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid Option, please try again.");
+                continue;
+            },
+        };
+
+        match choice {
+            1 => add_expense(),
+            2 => display_expenses(),
+            3 => transaction_history(),
+            4 => {
+                println!("\nThank you, bye!");
+                break;
+            },
+            _ => println!("\nInvalid number option, try again"),
+        }
+        // let 
+    }
 }
 
 fn add_expense() {
