2ed8e48f1ea01ce064512f5a689ae3984f398c7d
jacob <romory@pop-os.CG-24D>
2025-10-18 14:01:32 +0800

    Rust Slice done.

--- beginner/day_7/slice_type/src/main.rs
+++ beginner/day_7/slice_type/src/main.rs
@@ -5,7 +5,7 @@
       [starting_index..ending_index],
       * starting_index: first position of the slice
       * ending_index: one more than the last position in the slice.
-    
+
     * In Rust, `..` is called a range syntax. If you want to start at index 0,
     you can drop the value before the two periods.
 
@@ -25,12 +25,32 @@ fn main() {
     let slices = &s[..13];
     println!("{slices}");
 
+    // [0..6] || [..6] is the same
+
     // Or this (see 4th bullet):
     let len = s.len();
-    
+
     let slice = &s[3..len];
     println!("{slice}");
 
     let slice = &s[3..]; // Will output the same with slice variable above.
     println!("{slice}");
+
+    // [3..len] and [3..] are the same
+
+    // With slicing, pag gusto mo from start to end, you can do this: [..]
+    let slice_sample: String = String::from("ereT");
+
+    let slice2 = &slice_sample[..];
+    println!("{slice2}");
+
+    // Other slices
+    // General Slices Type
+    let a = [6, 1, 7, 2, 3];
+
+    let arr_slice = &a[..];
+    let arr_slice2 = &a[1..3];
+    println!("{:?}", arr_slice);
+    println!("{:?}", arr_slice2);
+    assert_eq!(arr_slice, &[1, 3]);
 }
