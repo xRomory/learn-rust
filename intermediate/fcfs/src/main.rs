104f464c1fd14997aa8fa94f251f9bbf20c229f6
chayen <romory@pop-os.CG-24D>
2025-10-30 08:24:44 +0800

    Initial commit for FCFS

---
+++ intermediate/fcfs/src/main.rs
@@ -0,0 +1,20 @@
+/*
+ * FCFS (First-Come, First-Served) Scheduling Algorithm Implementation in Rust
+ *
+ * FCFS is a non-preemptive CPU scheduling algorithm
+ * where the process that arrives first in the ready queue
+ * is executed first by the CPU.
+ *
+ * Characteristics:
+ * Non-preemptive: Once a process starts, it runs to completion.
+ * Based on arrival time: The process with the earliest arrival time gets the CPU first.
+ * Simple and fair, but can lead to long waiiting times for shorter processes.
+ *
+ * This is the first activity during my OS course.
+ * * The goal for this project is to convert Java implementation to Rust
+ * * Learn more and dig deeper to Rust
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
