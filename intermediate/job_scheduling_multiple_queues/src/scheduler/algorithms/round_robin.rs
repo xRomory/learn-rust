73dcd8f97bd1ebd5e285c21b5a23cfd022bd3677
chayen <romory@pop-os.CG-24D>
2026-03-20 15:42:08 +0800

    Added Round Robin algorithm

--- intermediate/job_scheduling_multiple_queues/src/scheduler/algorithms/round_robin.rs
+++ intermediate/job_scheduling_multiple_queues/src/scheduler/algorithms/round_robin.rs
@@ -1,15 +1,46 @@
-use crate::models::rr_process::RoundRobinProcess;
+use std::collections::VecDeque;
+
+use crate::{
+  models::job::Job, 
+  scheduler::scheduler::Scheduler
+};
 
 pub struct RoundRobinScheduler {
-  pub processes: Vec<RoundRobinProcess>,
+  pub queue: VecDeque<Job>,
   pub time_quantum: u32,
 }
 
 impl RoundRobinScheduler {
   pub fn new(time_quantum: u32) -> Self {
     RoundRobinScheduler { 
-      processes: Vec::new(), 
+      queue: VecDeque::new(), 
       time_quantum
+    }
+  }
+}
+
+impl Scheduler for RoundRobinScheduler {
+  fn add_job(&mut self, job: Job) {
+    self.queue.push_back(job);
+  }
+
+  fn schedule(
+    &mut self,
+    current_time: u32
+  ) -> Option<Job> {
+    if let Some(mut job) = self.queue.pop_front() {
+      let run_time = job.remaining_time.min(self.time_quantum);
+      job.remaining_time -= run_time;
+
+      if job.remaining_time > 0 {
+        self.queue.push_back(job);
+      } else {
+        job.completion_time = Some(current_time + run_time);
+      }
+
+      Some(job)
+    } else {
+      None
     }
   }
 }
