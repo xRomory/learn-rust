b24a2c3c30cf2e5618c0ce18f521213acf780d2b
chayen <romory@pop-os.CG-24D>
2026-04-05 20:19:46 +0800

    Added each queue has it's own scheduling algo

--- intermediate/job_scheduling_multiple_queues/src/models/mod.rs
+++ intermediate/job_scheduling_multiple_queues/src/models/mod.rs
@@ -1,3 +1,1 @@
 pub mod job;
-pub mod base_process;
-pub mod rr_process;
--- intermediate/job_scheduling_multiple_queues/src/queues/queue.rs
+++ intermediate/job_scheduling_multiple_queues/src/queues/queue.rs
@@ -1,33 +1,39 @@
-use std::collections::VecDeque;
 use crate::models::job::Job;
+use crate::scheduler::algorithms;
+use crate::scheduler::scheduler::Scheduler;
+
+pub enum QueueScheduler {
+  RoundRobin(algorithms::round_robin::RoundRobinScheduler),
+  FCFS(algorithms::fcfs::FCFSScheduler),
+}
 
 pub struct JobQueue {
   pub level: u8,
-  pub time_slice: Option<u32>,
-  pub queue: VecDeque<Job>
+  pub scheduler: QueueScheduler
 }
 
 impl JobQueue {
-  pub fn new(
-    level: u8,
-    time_slice: Option<u32>,
-  ) -> Self {
-    Self {
-      level,
-      time_slice,
-      queue: VecDeque::new()
+  pub fn add_job(&mut self, job: Job) {
+    match &mut self.scheduler {
+      QueueScheduler::RoundRobin(sched) => sched.add_job(job),
+      QueueScheduler::FCFS(sched) => sched.add_job(job),
     }
   }
 
-  pub fn enqueue(&mut self, job: Job) {
-    self.queue.push_back(job);
+  pub fn schedule(
+    &mut self,
+    current_time: u32,
+  ) -> Option<Job> {
+    match &mut self.scheduler {
+      QueueScheduler::RoundRobin(sched) => sched.schedule(current_time),
+      QueueScheduler::FCFS(sched) => sched.schedule(current_time),
+    }
   }
 
-  pub fn dequeue(&mut self) -> Option<Job> {
-    self.queue.pop_front()
-  }
-
-  pub fn job_is_empty(&mut self) -> bool {
-    self.queue.is_empty()
+  pub fn job_is_empty(&self) -> bool {
+    match &self.scheduler {
+      QueueScheduler::RoundRobin(sched) => sched.queue.is_empty(),
+      QueueScheduler::FCFS(sched) => sched.jobs.is_empty(),
+    }
   }
 }
