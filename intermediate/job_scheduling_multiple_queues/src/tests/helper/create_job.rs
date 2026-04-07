77c239a005c89c167d68f5bca5eedbadd0d47087
chayen <romory@pop-os.CG-24D>
2026-04-07 13:54:06 +0800

    Added MLQ scheduling unit test

--- intermediate/job_scheduling_multiple_queues/src/tests/helper/create_job.rs
+++ intermediate/job_scheduling_multiple_queues/src/tests/helper/create_job.rs
@@ -17,3 +17,22 @@ pub fn create_job(
     completion_time: None
   }
 }
+
+pub fn create_job_mlq(
+  id: u32,
+  arrival_time: u32,
+  burst_time: u32,
+  job_type: JobType,
+  priority: u8
+) -> Job {
+  Job {
+    id,
+    arrival_time,
+    burst_time,
+    remaining_time: burst_time, 
+    priority,
+    job_type,
+    start_time: None,
+    completion_time: None
+  }
+}
